d62c530aadc1a65de07670784167e0dd68bdbf28
Edward Matthew <edwardmk2704@gmail.com>
2020-12-22 02:42:40 +0700

    updated main

--- main_functions.py
+++ main_functions.py
@@ -8,4 +8,35 @@ import datetime
 import time
 import voice_functions as vf
 import web_automation as wa
-import greetings
+import greetings as greetings
+
+def initialize_mustard():
+    greetings.wish_me()
+    greetings.ask_for_name()
+
+    while True:
+        """Looping the engine"""
+        command = vf.take_command().lower()
+
+        if "play a youtube video" in command:
+            vf.speak("what should I play?")
+            wa.play_youtube_vid()
+
+        elif "how are you" in command:
+            vf.speak("I am fine, thank you for asking!")
+
+        elif "search google" in command:
+            vf.speak("what would you like me to search?")
+            wa.search_google()
+
+        elif "the time" or "what time is it" in command:
+            time_now = datetime.datetime.now().strftime("%H : %M : %S")
+            print(time_now)
+            vf.speak(f"the time is {time_now}")
+
+        elif "thanks mustard" or "thank you mustard" in command:
+            vf.speak("shutting down...")
+            print("Shutting down...")
+            exit()
+
+initialize_mustard()
