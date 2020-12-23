6cd3d35fa303b9f8ed1c477159b46f9faf6786ae
Edward Matthew <edwardmk2704@gmail.com>
2020-12-23 19:42:25 +0700

    added note function

--- file_automation.py
+++ file_automation.py
@@ -1,2 +1,13 @@
 import os
-import shutil
+import subprocess
+import datetime
+import voice_functions as vf
+
+def note(text):
+    """Creating a note"""
+    date = datetime.datetime.now()
+    file_name = str(date).replace(":", "-") + " - note.txt" # changing the naming convention to proper format
+    with open(file_name, "w") as new_file:
+        new_file.write(text)
+
+    subprocess.Popen(["notepad.exe", file_name])
