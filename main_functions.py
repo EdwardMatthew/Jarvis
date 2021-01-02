2a6947f1006b87b9e5b3ec7ede9dbde6622333fb
Edward Matthew <edwardmk2704@gmail.com>
2021-01-03 01:54:56 +0700

    changed google and youtube functions

--- main_functions.py
+++ main_functions.py
@@ -1,54 +1,64 @@
-import pyttsx3
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 import datetime
-import time
 import pyjokes
-import wikipedia
 import subprocess
 import voice_functions as vf
 import web_automation as wa
-import greetings as greetings
-import files
-import shutil
-import pprint
+import greetings
+import file_automation as fa
+import textwrap
+import webbrowser
+import os
+import winshell
 
-def initialize_mustard():
-    greetings.wish_me()
-    greetings.ask_for_name()
+def initialize_pickle():
+    """Main engine"""
 
-    active = True
+    command = vf.take_command().lower()
 
-    while active:
-        """Looping the engine"""
-        command = vf.take_command().lower()
+    if "play youtube" in command:
+        vf.speak("what should I play?")
+        query = vf.take_command().lower().replace(" ", "+")
+        wa.play_youtube_vid(query)
 
-        if "play youtube" in command:
-            vf.speak("what should I play?")
-            wa.play_youtube_vid()
+    elif "how are you" in command:
+        vf.speak("I am fine, thank you for asking!")
 
-        elif "how are you" in command:
-            vf.speak("I am fine, thank you for asking!")
+    elif "search google" in command:
+        vf.speak("what would you like me to search?")
+        query = vf.take_command().lower().replace(" ", "+")
+        wa.search_google(query)
 
-        elif "search google" in command:
-            vf.speak("what would you like me to search?")
-            wa.search_google()
+    elif "open google" in command:
+        webbrowser.open("https://google.com")
 
-        elif "time" in command:
-            time_now = datetime.datetime.now().strftime("%H : %M : %S")
+    elif "open google" in command:
+        webbrowser.open("https://youtube.com")
+
+    elif "time" in command:
+        time_now = datetime.datetime.now().strftime("%H : %M : %S")
+        print(f"The time now is {time_now}")
+        time_sound = str(time_now.split(":")[0])
+
+        # Vars for the proper way of telling time
+        time_hour = str(time_now.split(":")[0])
+        time_minutes = str(time_now.split(":")[1])
+        time_seconds = str(time_now.split(":")[2])
+
+        # Checking for pm and am
+        if int(time_sound) < 12:
             print(time_now)
-            vf.speak(f"the time now is {time_now}")
+            vf.speak(time_hour + "A M" + time_minutes + time_seconds)
+        if int(time_sound) > 12:
+            print(time_now)
+            vf.speak(time_hour + "P M" + time_minutes + time_seconds)
 
-        elif "joke" in command:
+    elif "joke" in command:
             vf.speak(pyjokes.get_joke())
 
-        elif "good morning" in command:
+    elif "good morning" in command:
             vf.speak("good morning user! have a great day!")
 
-        elif command == "shutdown system":
+    elif command == "shutdown system":
             vf.speak("are you sure you want to shutdown windows?")
             print("YES to shutdown and NO to cancel")
             shut_down = vf.take_command().lower()
@@ -60,29 +70,63 @@ def initialize_mustard():
             if "NO" in shut_down:
                 vf.speak("shutdown aborted. please continue with your work!")
 
-        elif "wikipedia" in command:
-            vf.speak("what does the user want to know?")
-            search_results = wa.search_wikipedia()
-            pp = pprint.PrettyPrinter(width=78, compact=True)
-            pp.pprint(search_results)
-            vf.speak(f"According to wikipedia, {search_results}")
+    elif command == "restart system":
+        vf.speak("are you sure you want to restart windows?")
+        print("YES to restart and NO to cancel")
+        restart = vf.take_command().lower()
 
-        elif command == "make a note" or command == "write this down":
-            vf.speak("what would you like me to note down?")
-            new_note = vf.take_command().lower()
-            if new_note != "cancel":
-                files.note(new_note)
-                vf.speak("I've made a note for that")
-                print("Note successfully made!")
-            else:
-                quit(files.note)
+        # Shutdown procedure
+        if "YES" in restart:
+            vf.speak("restarting the system, please wait a second...")
+            subprocess.call(["shutdown", "/r"])
+        if "NO" in restart:
+            vf.speak("restart aborted. please continue with your work!")
 
-        elif "I'm done" or "Thank you" in command:
-            print("Shutting down...")
-            vf.speak("shutting down...")
-            exit()
+    elif "wikipedia" in command:
+        vf.speak("what does the user want to know?")
+        search_results = wa.search_wikipedia()
+        for words in textwrap.wrap(search_results, 80):
+            print(words)
+        vf.speak(f"according to wikipedia, {search_results}")
 
+    elif "open outlook" in command:
+        print("Opening Outlook...")
+        vf.speak("opening outlook...")
+        webbrowser.open("https://outlook.com")
+
+
+    elif command == "make a note" or command == "write this down":
+        vf.speak("what would you like me to note down?")
+        new_note = vf.take_command().lower()
+        if new_note != "cancel":
+            fa.note(new_note)
+            vf.speak("I've made a note for that")
+            print("Note successfully made!")
         else:
-            vf.speak("command not recognized. Please try again")
+            print("Cancelling...")
+            vf.speak("process canceled")
+            print("Cancelled")
 
-initialize_mustard()
+    elif "empty recycle bin" in command:
+        winshell.recycle_bin().empty(confirm=False, show_progress= False)
+        vf.speak("recycle bin emptied!")
+        print("the recycle bin is emptied!")
+
+
+WAKE = "hey pickle"
+
+if __name__ == '__main__':
+    clear = lambda: os.system('cls')
+    clear()
+    greetings.wish_me()
+    greetings.ask_for_name()
+    initialize_pickle()
+
+    # waking the VA
+    while True:
+        activate = vf.take_command().lower()
+        if activate.count(WAKE) > 0:
+            vf.speak("I am ready")
+            initialize_pickle()
+        elif activate.count("turn off") > 0:
+            quit()
