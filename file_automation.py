16da56d7f43a05d62f132a8aa568816ed5663831
Edward Matthew <edwardmk2704@gmail.com>
2021-01-04 23:35:02 +0700

    updated functionality of note function

--- file_automation.py
+++ file_automation.py
@@ -1,13 +1,21 @@
 import os
 import subprocess
 import datetime
-import voice_functions as vf
 
 def note(text):
-    """Creating a note"""
+    """Creating a note
+    Change the filepath into your dedicated filepath if intending of using this"""
     date = datetime.datetime.now()
-    file_name = str(date).replace(":", "-") + " - note.txt" # changing the naming convention to proper format
-    with open(file_name, "w") as new_file:
+    # changing the name of the file
+    file_name = str(date).replace(":", "-") + " - note.txt"
+
+    # checking for dedicated file path, make one if doesn't exist
+    filepath = os.path.join("C:/Users/edwar/PycharmProjects/Jarvis/notes", file_name)
+    if not os.path.exists("C:/Users/edwar/PycharmProjects/Jarvis/notes"):
+        os.makedirs("C:/Users/edwar/PycharmProjects/Jarvis/notes")
+
+    # make the note in the right directory
+    with open(filepath, "w") as new_file:
         new_file.write(text)
 
-    subprocess.Popen(["notepad.exe", file_name])
+    subprocess.Popen(["notepad.exe", filepath])
